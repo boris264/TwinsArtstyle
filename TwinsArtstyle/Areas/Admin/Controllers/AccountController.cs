d039ffdf4b85900edf19a924a833ff3aa10c0435
boris264 <boristodorov12@gmail.com>
2022-03-18 12:20:43 +0200

    Added Admin Layout and Login view for the admin area.

---
+++ TwinsArtstyle/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwinsArtstyle.Areas.Admin.Controllers
+{
+    public class AccountController : AdminController
+    {
+        public IActionResult Login() => View();
+    }
+}
---
+++ TwinsArtstyle/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwinsArtstyle.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+
+    }
+}
